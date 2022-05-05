7bf73ea209ce69b3b47ffd7f291a76f4bcfcde09
Ahmet <aegoksu@gmail.com>
2022-05-05 12:23:33 +0300

    Initial Commit

---
+++ Sayaclar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sayaclar
+//
+//  Created by AHMET on 5.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
